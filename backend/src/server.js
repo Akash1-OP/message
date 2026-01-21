732fa76a391e290991f090a43aedc7b67c4085ee
Akash1-OP <akashsahana62@gmail.com>
2026-01-21 21:54:10 +0530

    almost complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from "path"
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
-dotenv.config();
 const __dirname = path.resolve();
 
-const app = express();
+const PORT = ENV.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
@@ -24,5 +29,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-app.listen(PORT,()=> console.log("server running on port " + PORT));
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
